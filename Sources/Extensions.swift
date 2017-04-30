5947408d642684d78391e50be5a3247bdfe2fd54
Fabio Tacke <tackefab@informatik.hu-berlin.de>
2017-04-30 10:49:31 +0200

    Split code into different files

---
+++ Sources/Extensions.swift
@@ -0,0 +1,39 @@
+//
+//  Extensions.swift
+//  PVSS.swift
+//
+//  Created by Fabio Tacke on 30.04.17.
+//
+//
+
+import Foundation
+
+import BigInt
+
+extension BigUInt {
+  
+  public static func randomPrime(length: Int) -> BigUInt {
+    var random = BigUInt.randomInteger(withMaximumWidth: length)
+    
+    while !random.isPrime() {
+      random = BigUInt.randomInteger(withMaximumWidth: length)
+    }
+    
+    return random
+  }
+}
+
+extension BigInt {
+  
+  public func mod(modulus: BigUInt) -> BigUInt {
+    let abs = self.abs
+    let div = abs.divided(by: modulus)
+    
+    if self.negative && div.remainder != 0 {
+      let factor = BigInt(abs: div.quotient + 1, negative: true)
+      return ((factor * BigInt(modulus)) - self).abs
+    } else {
+      return div.remainder
+    }
+  }
+}
